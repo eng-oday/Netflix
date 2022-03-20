4370541eba2578dacceaf9efce9dd49f9f82fbd2
OdayMohamed <odaydieg@gmail.com>
2022-03-20 13:58:25 +0200

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Oday Dieg on 20/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
